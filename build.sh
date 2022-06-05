b43cc6ee8948fcbc0bbedc4aaabbde5e254af1b8
Sergio Lima <sergiosouzalima@gmail.com>
2022-06-05 16:25:50 -0300

    Added newest Nim version number

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -u
 src_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source "$src_dir/scripts/installnim.sh"
 
-install_nim_check "0.19.4"
+install_nim_check "1.6.6"
 nim -v
 nim c -r "$src_dir/test.nim"
